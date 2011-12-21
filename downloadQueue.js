762324922be570f51aaade7f4a305e061afdff1c
Rob Griffiths <rob@bytespider.eu>
2011-12-21 22:30:28 +0000

    added resumable downloads and fixed progress indicator issues

--- downloadQueue.js
+++ downloadQueue.js
@@ -1,7 +1,9 @@
 var queue = [], httpConnections = 0;
 
+var downloads = JSON.parse(Ti.App.Properties.getString("downloadQueue")) || {};
+
 exports.maxConnections = 4;
-exports.downloadDirectory = Ti.Filesystem.tempDirectory;
+exports.downloadDirectory = Ti.Filesystem.applicationDataDirectory;
 exports.timeout = 30000;
 
 exports.add = function (fileDescriptor)
@@ -26,18 +28,100 @@ exports.process = function ()
     {
         httpConnections++;
         
+        var directory = Ti.Filesystem.getFile(dq.downloadDirectory);
+        if (!directory.exists())
+        {
+            directory.createDirectory();
+        }
+        
+        var hash = Ti.Utils.md5HexDigest(dq.downloadDirectory + item.filename);
+        if (!(hash in downloads))
+        {
+            downloads[hash] = {
+                path: dq.downloadDirectory + item.filename,
+                filename: item.filename,
+                completeSize: 0
+            };
+        }
+        Ti.App.Properties.setString("downloadQueue", JSON.stringify(downloads));
+        
         var file = Ti.Filesystem.getFile(dq.downloadDirectory + item.filename);
+        if (file.exists() && (downloads[hash].completeSize > 0 && file.size == downloads[hash].completeSize))
+        {
+            if (typeof item.progress == "function")
+            {
+                item.progress({progress: 1});                        
+            }
+            
+            Ti.fireEvent("downloadqueue:complete", item);
+            item.complete(null);
+            
+            if (queue.length == 0)
+            {
+                Ti.fireEvent("downloadqueue:queuecomplete", dq);
+            }
+            continue;
+        } 
+        else
+        {
+            file.createFile();
+        }
         item.file = file;
+        item.hash = hash;
         
         var conenction = Ti.Network.createHTTPClient({
             timeout: dq.timeout,
-            file: file,
+            //file: file,
 
             ondatastream: (function (fileDescriptor)
             {
+                var sessionPointer = 0;
+                var outstream = fileDescriptor.file.open(Titanium.Filesystem.MODE_APPEND);
+                
                 return function (event)
                 {
-                    fileDescriptor.progress(event);
+                    var content_length = this.getResponseHeader("Content-Length"),
+                        hash = fileDescriptor.hash,
+                        file = fileDescriptor.file,
+                        downloadedSize = this.responseData.length;
+                    
+                    if (downloads[hash].completeSize == 0 && content_length > 0)
+                    {
+                        downloads[hash].completeSize = content_length;
+                        Ti.App.Properties.setString("downloadQueue", JSON.stringify(downloads));
+                    }
+                    
+                    Ti.API.log("content-size: " + downloads[hash].completeSize);
+                    
+                    var buffer = Ti.createBuffer({length: downloadedSize});
+                    var instream = Titanium.Stream.createStream({mode: Titanium.Stream.MODE_READ, source: this.responseData});
+                    
+                    
+                    // Read and write chunks.
+                    instream.read(buffer);
+                    outstream.write(buffer, sessionPointer, downloadedSize);
+                    
+                    instream.close();
+                    
+                    sessionPointer = downloadedSize;
+                    Ti.API.log("session pointer: " + sessionPointer);
+                    
+                    // override progress using file size
+                    Ti.API.log(event.progress);
+                    Ti.API.log(file.size / downloads[hash].completeSize);
+                    Ti.API.log("file size: "  + file.size);
+                    
+                    event.progress = file.size / downloads[hash].completeSize;
+
+                    if (typeof fileDescriptor.progress == "function")
+                    {
+                        fileDescriptor.progress(event);                        
+                    }
+                    
+                    if (event.progress == 1)
+                    {
+                        outstream.close();
+                    }
                 }
             })(item),
 
@@ -45,7 +129,10 @@ exports.process = function ()
             {
                 return function (event)
                 {
+                    fileDescriptor.file.write(this.responsData);
+                    
                     Ti.fireEvent("downloadqueue:complete", fileDescriptor);
+                    fileDescriptor.complete(event);
                     httpConnections--;
 
                     if (dq.queue.length > 0)
@@ -68,6 +155,12 @@ exports.process = function ()
         });
         
         conenction.open("GET", item.url);
+        
+        if (file.size > 0 && file.size != downloads[hash].completeSize)
+        {
+            conenction.setRequestHeader("Range", "bytes=" + file.size + "-");
+        }
+        
         conenction.send();
     }
 };
